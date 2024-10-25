44ea63f6850eb614f2dbf522811606bb3eb2d237
Wal Monfortt <jose.gual.monfortte.f@gmail.com>
2024-10-24 18:35:10 -0600

    bill calculated v1

---
+++ Tipsy/Controllers/CalculatorViewController.swift
@@ -0,0 +1,76 @@
+//
+//  ViewController.swift
+//  Tipsy
+//
+//  Created by Angela Yu on 09/09/2019.
+//  Copyright © 2019 The App Brewery. All rights reserved.
+//
+
+import UIKit
+
+class CalculatorViewController: UIViewController {
+
+    @IBOutlet weak var zeroPrcView: UIButton!
+    @IBOutlet weak var tenPrcView: UIButton!
+    @IBOutlet weak var twentyPrcView: UIButton!
+    @IBOutlet weak var splitNumberLabel: UILabel!
+    
+    var btnSelected: String?
+    var people: Int?
+    var totalBill: Float?
+
+    @IBAction func percentagPressed(_ sender: UIButton) {
+        print("button pressed \(sender.titleLabel!.text!)")
+        sender.isSelected ? nil : sender.isSelected.toggle()
+        
+        btnSelected = sender.titleLabel!.text!
+        
+        unselectBtns()
+    }
+    
+    @IBAction func getTotalBill(_ sender: UITextField) {
+        print(sender.text!)
+        totalBill = Float(sender.text!)
+    }
+
+    func unselectBtns() {
+        let btns: [UIButton] = [zeroPrcView, tenPrcView, twentyPrcView]
+        for btn in btns {
+            if btn.titleLabel!.text! != btnSelected{
+                btn.isSelected = false
+            }
+        }
+    }
+    
+    @IBAction func stepperValueChanged(_ sender: UIStepper) {
+        splitNumberLabel.text = String(Int(sender.value).description)
+        people = Int(sender.value)
+    }
+
+    @IBAction func calculatePressed(_ sender: UIButton) {
+        if btnSelected == nil {
+            btnSelected = "10%"
+        }
+        if people == nil {
+            people = 2
+        }
+        let index = btnSelected!.firstIndex(of: "%") ?? btnSelected!.endIndex
+        let percentageString = btnSelected![..<index]
+        let percentage = Double(percentageString)! / 100
+        print(percentage)
+        print("people: \(people!)")
+        print(totalBill!)
+        
+        calculateAmount(Float(people!), percentage, totalBill!)
+
+    }
+    
+    func calculateAmount(_ people: Float, _ percentage: Double, _ totalAmount: Float) {
+        let amount = (totalAmount + (totalAmount * Float(percentage)));
+        let finalAmount = amount / Float(people)
+        print("the final amount is: \(finalAmount)")
+        
+    }
+    
+}
+
---
+++ Tipsy/Controllers/ResultViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ResultViewController.swift
+//  Tipsy
+//
+//  Created by Jose Gualberto Monfortte Flores on 24/10/24.
+//  Copyright © 2024 The App Brewery. All rights reserved.
+//
+
+import UIKit
+
+class ResultViewController: UIViewController {
+
+    @IBOutlet weak var totalLabel: UILabel!
+    @IBOutlet weak var settingsLabel: UILabel!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    @IBAction func recalculatePressed(_ sender: UIButton) {
+    }
+    
+}
